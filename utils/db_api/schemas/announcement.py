c031ac1018fd0aeac1a13f6d5cec28d533dceb25
Aldiyar58 <u.aldiyarkz@gmail.com>
2023-09-21 21:42:07 +0600

    add in db 4 tyes

--- utils/db_api/schemas/announcement.py
+++ utils/db_api/schemas/announcement.py
@@ -9,3 +9,27 @@ class Fill_Ad_Event(TimedBaseModel):
     time_event = Column(String(50))
     description_event = Column(String(3000))
     query: sql.select
+
+
+class Fill_Ad_Lost(TimedBaseModel):
+    __tablename__ = 'fill_Ad_Lost'
+    photo = Column(Text)
+    date_lost = Column(String(200))
+    description_item = Column(String(3000))
+    query: sql.select
+
+
+class Event_without_photo(TimedBaseModel):
+    __tablename__ = 'event_without_photo'
+    date_event = Column(String(200))
+    time_event = Column(String(50))
+    description_event = Column(String(3000))
+    query: sql.select
+
+
+class Fill_Ad_news(TimedBaseModel):
+    __tablename__ = 'fill_Ad_news'
+    news_text = Column(String(3000))
+    query: sql.select
+
+
